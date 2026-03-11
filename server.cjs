9661374fb3fda94e505cbb0cd9e752c62ecc2884
Vaishanavi Shelke <deshmukhrushikesh684@gmail.com>
2026-03-11 08:10:23 +0530

    fix server.cjs

--- server.cjs
+++ server.cjs
@@ -4,19 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-console.log('Starting server...');
-console.log('PORT:', PORT);
-console.log('dirname:', __dirname);
-
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Frontend running on port ${PORT}`);
-}).on('error', (err) => {
-    console.error('❌ Server failed to start:', err);
-    process.exit(1);
+  console.log(`✅ Frontend running on port ${PORT}`);
 });
